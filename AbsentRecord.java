e0a01c2d2997130c1d3f7aa984b04e567c9454ca
complex1661 <144210657+complex1661@users.noreply.github.com>
2024-05-22 01:21:17 +0800

    Add files via upload

---
+++ AbsentRecord.java
@@ -0,0 +1,53 @@
+package ems;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.time.*;
+public class AbsentRecord {
+  private Message detail;
+  private EDate startDate;
+  private EDate endDate;
+  public AbsentRecord (Message txt, EDate sd, EDate ed) throws IllegalArgumentException{
+    detail = txt;
+    if (isValidDates(sd, ed)) {
+      startDate = sd;
+      endDate = ed;
+    } else {
+      throw new IllegalArgumentException("錯誤的開始結束區間。");
+    }
+  }
+  
+  public boolean isValidDates(EDate start, EDate end) {
+    if (start.getYear() <= end.getYear() && start.getMonth() <= end.getMonth() && start.getDate() <= end.getDate()) {
+      return true;
+    }
+    else return false;
+  }
+  
+  public String getDetail() {
+    return detail.getDetail();
+  }
+  public int getYear() {
+    return startDate.getYear();
+  }
+  public int getMonth() {
+    return startDate.getMonth();
+  }
+  
+  public int getDurationWorkdays() {
+    Calendar start = startDate.createDate();
+    Calendar end = endDate.createDate(); 
+    int workdays = 0;
+    if (start.getTimeInMillis() == end.getTimeInMillis()) {
+        return 0;
+    }
+    do {
+      start.add(Calendar.DAY_OF_MONTH, 1);
+      if (start.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY && start.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
+        ++workdays;
+      } 
+    }
+    while (start.getTimeInMillis() < end.getTimeInMillis());
+    return workdays; 
+  }
+}
